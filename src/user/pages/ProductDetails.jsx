8af47bd982aef1c51e22836f2a0333243a852c98
jaggu0000 <sreejaganj@gmail.com>
2024-12-23 16:00:41 +0530

    added add to cart functionality

--- src/user/pages/ProductDetails.jsx
+++ src/user/pages/ProductDetails.jsx
@@ -1,14 +1,18 @@
-import React, { useEffect, useState } from 'react';
-import { NavLink, useParams } from 'react-router-dom';
+import React, { useContext, useEffect, useState } from 'react';
+import { NavLink, useNavigate, useParams } from 'react-router-dom';
 import UserNavbar from '../../components/UserNavbar';
 import Footer from '../../components/Footer';
 import { fetchProductById } from '../../api/productApi';
+import { CartContext } from '../../contexts/CartContext';
 
 const ProductDetails = () => {
     const { id } = useParams();
     const [product, setProduct] = useState(null);
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(null);
+    const { addToCart } = useContext(CartContext);
+    const user = localStorage.getItem("user");
+    const navigate = useNavigate;
 
     useEffect(() => {
         const fetchProductDetails = async () => {
@@ -63,13 +67,13 @@ const ProductDetails = () => {
                         <p className="text-green-600 text-xl font-semibold mb-2">₹{product.price}</p>
                         <p className="text-yellow-500 text-lg mb-4">Rating: {product.rating} ⭐</p>
 
-                        <div className='w-96 flex-col gap-6 mt-4 -ml-2'>
-                            <NavLink
-                                to={'/cart'}
+                        <div className=' flex w-96 flex-col gap-0 mt-4 -ml-2'>
+                            <button
+                                onClick={() => user? addToCart(product) : navigate("/cart")}
                                 className="flex mx-2 my-2 justify-center items-center py-2 bg-orange-600 hover:bg-orange-700 rounded-3xl"
                             >
                                 Add to cart
-                            </NavLink>
+                            </button>
                             <NavLink
                                 to={'/checkout'}
                                 className="flex mx-2 my-2 justify-center items-center py-2 bg-orange-600 hover:bg-orange-700 rounded-3xl"
