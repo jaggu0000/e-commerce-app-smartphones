9966b9c50f3b1246506be2c8ce3d808deaf32fc8
jaggu0000 <sreejaganj@gmail.com>
2024-12-24 18:01:25 +0530

    added a function to fetch all orders

--- src/api/orderApi.js
+++ src/api/orderApi.js
@@ -6,6 +6,10 @@ export const fetchOrderById = (id) => {
     return axios.get(`${ORDER_URL}?userId=${id}`);
 }
 
+export const fetchAllOrders = () => {
+    return axios.get(ORDER_URL);
+}
+
 export const addNewOrder = (newOrder) => {
     const response = axios.post(ORDER_URL, newOrder);
     return response.data;
